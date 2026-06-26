f4b6311946afe7ac5db671acea57c9c40dc5a45c
Joshua Chavez <jnchavezc@gmail.com>
2026-06-26 09:00:00 -0500

    chore: actualizar configuración base del frontend con Tailwind y entrada principal

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,13 @@ import "./app/globals.css";
 // import Layout from "./app/layout"; // Si usas Next.js el layout es distinto, pero en Vite/React puro se suele usar así:
 import Page from "./app/page";
 import { ThemeProvider } from "./components/theme-provider"; // <--- IMPORTAR
+import { Toaster } from "./components/ui/sonner";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider> {/* <--- ENVOLVER AQUÍ */}
       <Page />
+      <Toaster />
       {/* Si tu componente Page ya incluye el Sidebar y Layout, está bien. 
            Si Layout es el contenedor principal, úsalo: <Layout><Page /></Layout> */}
     </ThemeProvider>
